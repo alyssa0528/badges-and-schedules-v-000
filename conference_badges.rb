818cf47f9f069e3207154a54fa304cd67eae610c
Alyssa Kim <alyssa.kim@gmail.com>
2018-02-05 19:26:08 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,14 @@
 # Write your code here.
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(attendees)
+  attendees.each do |name|
+    "Hello, my name is #{name}."
+  end 
+end
+
+def assign_rooms(attendees)
+  
+end
