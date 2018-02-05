f018540db13645206629c523df955502c73099dd
Alyssa Kim <alyssa.kim@gmail.com>
2018-02-05 19:37:19 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -14,7 +14,7 @@ end
 def assign_rooms(attendees)
   attendees.each do |name|
   room_counter = 1 
-  "Hello, #{name}! You'll be assigned to room #{room-counter}!"  
+  "Hello, #{name}! You'll be assigned to room #{room_counter}!"  
     room_counter += 1 
   end 
 end
