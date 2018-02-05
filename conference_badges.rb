fa1fcb64466d057098d14f300442d6147aead321
Alyssa Kim <alyssa.kim@gmail.com>
2018-02-05 20:20:37 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -24,6 +24,24 @@ def assign_rooms(attendees)
 end
 
 def printer(attendees)
-  puts batch_badge_creator(attendees).each
-  puts assign_rooms(attendees).each 
+  batch_badge_creator(attendees)
+  badge_messages = []
+  counter = 0 
+  attendees.each do |name|
+  badge_messages[counter] = badge_maker(name)
+  counter += 1 
+  end 
+  badge_messages.each do |badge|
+    puts badge
+  end
+  assign_rooms(attendees)
+  room_assignments = []
+  counter = 0 
+  attendees.each_with_index do |name, index|
+  room_assignments[counter] = "Hello, #{name}! You'll be assigned to room #{index + 1}!"  
+  counter += 1 
+  end 
+  room_assignments.each do |assignment|
+    puts assignment 
+  end
 end 
