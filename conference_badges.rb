456a6faffb2378ce47a1a6e2bc0f8fdbbc808b24
Alyssa Kim <alyssa.kim@gmail.com>
2018-02-05 20:16:59 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -26,8 +26,6 @@ end
 def printer(attendees)
   puts batch_badge_creator(attendees).each do |badge|
   end 
-  assign_rooms(attendees)
-    room_assignments.each do |assignment|
-    puts assignment
+  puts assign_rooms(attendees).each do |assignment|
   end
 end 
